7e7fd1bc2a25033fb975e4eadd1ee8ebf87f5bbb
Kevin Juan Jacque Castillo <kevinjuanjacquecastillo@MacBook-de-Kevin.local>
2020-10-21 00:44:34 -0300

    agregando cv visible

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import render_template 
 from flask import request
+import os
+
+
+uploads_dir = os.path.join('static')
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    print(uploads_dir )
     return render_template("home.html")
 
 @app.route('/kevin')
@@ -15,6 +20,8 @@ def kevin():
 def ignacio():
     return render_template('ignacio.html')
 
+
+
 @app.route('/nuevo-integrante', methods=['POST'])
 def NuevoIntegrante():
     nombreCompleto = request.form['nombreCompleto']
@@ -22,8 +29,15 @@ def NuevoIntegrante():
     Facebook = request.form['Facebook']
     Twitter = request.form['Twitter']
     GitHub = request.form['GitHub']
+    CV =request.files['SubirCurriculum']
+    CV.save(os.path.join(uploads_dir, 'CVNuevo.pdf'))
 
     return render_template('NuevoIntegrante.html', nombreCompleto = nombreCompleto, Carrera = Carrera, Facebook = Facebook , Twitter = Twitter, GitHub = GitHub)
+
+@app.route('/CVNuevo')
+def CVNuevo():
+    return render_template('CVNuevo.html')
+
 
 if __name__ == '__main__':
     
